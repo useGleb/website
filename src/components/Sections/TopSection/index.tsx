f13ae054faf9aab9abfbf49e2f2f6f32bbd3ae00
useGlebKolosov <123364201+useGlebKolosov@users.noreply.github.com>
2024-11-30 10:24:19 +0200

    test

--- src/components/Sections/TopSection/index.tsx
+++ src/components/Sections/TopSection/index.tsx
@@ -11,7 +11,13 @@ const TopSection = ({ className }: TopSectionProps) => {
   return (
     <div className={className}>
       <LanguageBar />
-      <Image src='assets/test.jpg' width={100} height={100} alt='123123' />
+      <Image
+        src='assets/test.jpg'
+        width='0'
+        height='0'
+        sizes='100vw'
+        alt='123123'
+      />
       <Image src='test.jpg' width={100} height={100} alt='123123' />
       <div className='font-bold text-3xl lg:text-7xl text-typography'>
         {t('titleFirstLine')}
